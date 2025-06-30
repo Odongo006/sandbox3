289627637b1ea6e0d8d3dd50a3231fb54b1565ae
Henry Odongo <www.henryodongo006@gmail.com>
2025-06-30 21:24:15 +0300

    Update middlewares.generateAccessToken.js

--- middlewares/middlewares.generateAccessToken.js
+++ middlewares/middlewares.generateAccessToken.js
@@ -1,39 +1,46 @@
 import request from "request";
-import 'dotenv/config'
+import 'dotenv/config';
 
-export const accessToken = (req, res, next)=> {
-    try{
+export const accessToken = (req, res, next) => {
+    const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+    const auth = Buffer.from(`${process.env.SAFARICOM_CONSUMER_KEY}:${process.env.SAFARICOM_CONSUMER_SECRET}`).toString('base64');
 
-        const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-        const auth = new Buffer.from(`${process.env.SAFARICOM_CONSUMER_KEY}:${process.env.SAFARICOM_CONSUMER_SECRET}`).toString('base64');
-
-        request(
-            {
-                url: url,
-                headers: {
-                    "Authorization": "Basic " + auth
-                }
-            },
-            (error, response, body) => {
-                if (error) {
-                    res.status(401).send({
-                        "message": 'Something went wrong when trying to process your payment',
-                        "error":error.message
-                    })
-                }
-                else {
-                    req.safaricom_access_token = JSON.parse(body).access_token
-                    next()
-                }
+    request(
+        {
+            url: url,
+            headers: {
+                "Authorization": "Basic " + auth
             }
-        )
-    }catch (error) {
+        },
+        (error, response, body) => {
+            if (error) {
+                console.error("Access token request error:", error);
+                return res.status(500).json({
+                    message: "Something went wrong when trying to process your payment",
+                    error: error.message
+                });
+            }
 
-        console.error("Access token error ", error)
-        res.status(401).send({
-            "message": 'Something went wrong when trying to process your payment',
-            "error":error.message
-        })
-    }
+            try {
+                if (!body) {
+                    throw new Error("Empty response body from Safaricom OAuth");
+                }
 
-}
+                const data = JSON.parse(body);
+
+                if (!data.access_token) {
+                    throw new Error("Access token not found in response");
+                }
+
+                req.safaricom_access_token = data.access_token;
+                next();
+            } catch (parseError) {
+                console.error("Access token parsing error:", body);
+                return res.status(500).json({
+                    message: "Failed to parse access token response",
+                    error: parseError.message
+                });
+            }
+        }
+    );
+};
