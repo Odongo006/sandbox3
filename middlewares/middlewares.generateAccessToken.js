52185235ec1a9f484261637373df13fb8f6194d4
Henry Odongo <www.henryodongo006@gmail.com>
2025-07-04 22:52:37 +0300

    Update middlewares.generateAccessToken.js

--- middlewares/middlewares.generateAccessToken.js
+++ middlewares/middlewares.generateAccessToken.js
@@ -2,7 +2,7 @@ import request from "request";
 import 'dotenv/config';
 
 export const accessToken = (req, res, next) => {
-    const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+    const url = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
     const auth = Buffer.from(`${process.env.SAFARICOM_CONSUMER_KEY}:${process.env.SAFARICOM_CONSUMER_SECRET}`).toString('base64');
 
     request(
