0f8c5161a92b6e6232e66acae3198fe19829c077
Alpha7 <henryomondi6612@gmail.com>
2025-08-01 23:31:08 +0300

    Case sensitive

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/userSchema.js";
 import ErrorHandler from "../middlewares/error.js";
-import { sendToken } from "../utils/jwtToken.js";
+import { sendToken } from "../Utils/jwtToken.js";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
   const { name, email, phone, password, role } = req.body;
